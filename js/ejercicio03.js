4152aa81b9e983737720b5ffdb9e0b66b67e4cfc
Silvio Ruiz Cormack <silviojrck@gmail.com>
2023-03-03 00:53:20 -0300

    Agregando logica ejercicio03

--- js/ejercicio03.js
+++ js/ejercicio03.js
@@ -1,3 +1,16 @@
 function ejercicio03(el){
-    
+    const articleEl = document.createElement("article");
+    articleEl.innerHTML = `
+    <p class="resultado"></p>
+    `
+    const resultado = articleEl.querySelector(".resultado");
+    let palabra = "";
+    do {
+         palabra = prompt("Agregue una palabra")
+    } while (confirm("Desea seguir agregando palabras?") == true);
+
+    resultado.textContent=`${palabra}`
+
+    el.appendChild(articleEl);
+
 }
