eb8dc5b24c4817a9967c413d14c68b3d683ede3a
Silvio Ruiz Cormack <silviojrck@gmail.com>
2023-03-03 01:40:40 -0300

    Agregando logica ejercicio 5

--- js/ejercicio05.js
+++ js/ejercicio05.js
@@ -1,3 +1,17 @@
-function ejercicio05(el){
-    
+function ejercicio05(el) {
+  const articleEl = document.createElement("article");
+  articleEl.innerHTML = `
+   <p class="resultado"></p>
+   `;
+  const letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+  do {
+    const dni = parseInt(prompt("Ingrese un DNI"));
+    if (!isNaN(dni) && dni >= 0 && dni <= 99999999) {
+        let letra = letras.charAt(dni % 23);
+        alert("La letra de tu DNI es: " + letra);
+    } else {
+        alert("Debe ingresar un numero de dni valido!");
+    }
+  } while (confirm("Desea seguir calculando letras de DNI?"));
 }
+
