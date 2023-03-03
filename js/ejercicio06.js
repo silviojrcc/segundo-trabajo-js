b1b09a098876879ec897dbf562785b375d404b57
Silvio Ruiz Cormack <silviojrck@gmail.com>
2023-03-03 01:58:26 -0300

    Agregando logica ejercicio 6

--- js/ejercicio06.js
+++ js/ejercicio06.js
@@ -1,3 +1,19 @@
 function ejercicio06(el){
-    
+    const articleEl = document.createElement("article");
+    articleEl.innerHTML = `
+    <h2>Ejercicio 6</h2>
+    <div class="resultado"></div>
+    `
+    const resultado = articleEl.querySelector(".resultado");
+    let texto = "";
+
+    for (let i = 1; i <= 30 ; i++) {
+        for (let j = 1; j <= i; j++) {
+            texto += `${i}`
+        }
+        texto += `<br>`
+    }
+
+    resultado.innerHTML = texto;
+    el.appendChild(articleEl);
 }
