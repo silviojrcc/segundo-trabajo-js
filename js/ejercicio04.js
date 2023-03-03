253799574a5d90fe25b45cc0b9291c6ee1ec8d3a
Silvio Ruiz Cormack <silviojrck@gmail.com>
2023-03-03 01:24:19 -0300

    Agregando logica ejercicio04

--- js/ejercicio04.js
+++ js/ejercicio04.js
@@ -1,3 +1,21 @@
 function ejercicio04(el){
+    const articleEl = document.createElement("article");
+    articleEl.innerHTML = `
+    <p class="resultado"></p>
+    `
+    const resultado = articleEl.querySelector(".resultado");
+    let suma = 0;
 
+    do {
+        const numero = parseFloat(prompt("Ingrese un numero"));
+        if (!isNaN(numero)) {
+            suma += numero;
+        } else {
+            alert("Debe ingresar un numero valido!!");
+        }
+    } while (confirm("Desea seguir agregando numeros?") == true);
+
+    resultado.textContent = `La suma de todos los numeros ingresados dio ${suma}`;
+    
+    el.appendChild(articleEl);
 }
