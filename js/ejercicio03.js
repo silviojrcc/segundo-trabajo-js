e72b2e188c1ace703d723a69c3813277d948dcb3
Silvio Ruiz Cormack <silviojrck@gmail.com>
2023-03-03 01:07:43 -0300

    Terminando ejercicio03

--- js/ejercicio03.js
+++ js/ejercicio03.js
@@ -6,10 +6,11 @@ function ejercicio03(el){
     const resultado = articleEl.querySelector(".resultado");
     let palabra = "";
     do {
-         palabra = prompt("Agregue una palabra")
+        palabra += prompt("Agregue una palabra");
+        palabra += " - "
     } while (confirm("Desea seguir agregando palabras?") == true);
 
-    resultado.textContent=`${palabra}`
+    resultado.textContent=`${palabra.slice(0, -2)}`
 
     el.appendChild(articleEl);
 
