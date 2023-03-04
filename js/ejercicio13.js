03a2173f283b0ef251980b756c90f7b50ca7d75a
Silvio Ruiz Cormack <silviojrck@gmail.com>
2023-03-03 23:54:58 -0300

    agregando logica ejercicio13

--- js/ejercicio13.js
+++ js/ejercicio13.js
@@ -1,3 +1,30 @@
 function ejercicio13(el){
-    
+    const articleEl = document.createElement("article");
+    articleEl.innerHTML = `
+    <h2>Ejercicio 13</h2>
+    <form class="form">
+        <div>
+            <label for="texto">Ingrese un texto</label>
+            <input class="texto" name="texto" type="text">
+        </div>
+        <button>Convertir a mayúsculas!</button>
+    </form>
+    <p class="resultado"></p>
+    `;
+
+    const form = articleEl.querySelector(".form");
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const texto = articleEl.querySelector(".texto").value;
+        const resultadoEl = articleEl.querySelector(".resultado");
+
+        if (texto != "") {
+            resultadoEl.textContent = texto.toUpperCase();
+        } else {
+            resultadoEl.textContent = "Debe ingresar una palabra válida!!";
+        }
+        
+    });
+    el.appendChild(articleEl);
 }
