52d5b7d3cecc48945b586e29d20b98c0ea64e03e
Silvio Ruiz Cormack <silviojrck@gmail.com>
2023-03-04 01:10:57 -0300

    agregando logica ejercicio16

--- js/ejercicio16.js
+++ js/ejercicio16.js
@@ -1,3 +1,28 @@
 function ejercicio16(el){
-    
+    const articleEl = document.createElement("article");
+    articleEl.innerHTML = `
+    <h2>Ejercicio 16</h2>
+    <form class="form">
+        <div>
+            <label for="frase">Ingresar una frase</label>
+            <input class="frase" name="frase" type="text">
+        </div>
+        <button>Dar vuelta la frase!</button>
+    </form>
+    <p class="resultado"></p>
+    `;
+
+    const form = articleEl.querySelector(".form");
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const resultadoEl = articleEl.querySelector(".resultado");
+        const frase = articleEl.querySelector(".frase").value;
+        let fraseAlReves = "";
+
+        for (let i = frase.length; i >= 0; i--) {
+            fraseAlReves += frase.charAt(i);
+        }
+        resultadoEl.textContent = fraseAlReves;
+    });
+    el.appendChild(articleEl);
 }
