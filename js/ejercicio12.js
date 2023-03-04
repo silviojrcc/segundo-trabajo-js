e8bb65191e7d0c8b279e8137c5446aa3bff0db5f
Silvio Ruiz Cormack <silviojrck@gmail.com>
2023-03-03 23:40:21 -0300

    Agregando logica ejercicio12

--- js/ejercicio12.js
+++ js/ejercicio12.js
@@ -1,3 +1,20 @@
 function ejercicio12(el){
-    
+    const articleEl = document.createElement("article");
+    articleEl.innerHTML = `
+    <h2>Ejercicio 12</h2>
+    <button class="boton">Generar número aleatorio</button>
+    <p class="resultado"></p>
+    `;
+
+    const boton = articleEl.querySelector(".boton");
+    const resultadoEl = articleEl.querySelector(".resultado");
+
+    const numeroMinimo = 1;
+    const numeroMaximo = 99;
+
+    boton.addEventListener("click", () => {
+        const numeroAleatorio = Math.floor(Math.random() * (numeroMaximo - numeroMinimo + 1) + numeroMinimo);
+        resultadoEl.textContent = numeroAleatorio;
+    });
+    el.appendChild(articleEl);
 }
