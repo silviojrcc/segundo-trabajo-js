fe2145e51a1811854da1b59fe6eba876b270ef15
Silvio Ruiz Cormack <silviojrck@gmail.com>
2023-03-04 02:10:14 -0300

    agregando titulo y validaciones a ejercicio03

--- js/ejercicio03.js
+++ js/ejercicio03.js
@@ -1,16 +1,25 @@
 function ejercicio03(el){
     const articleEl = document.createElement("article");
     articleEl.innerHTML = `
+    <h2>Ejercicio 3</h2>
     <p class="resultado"></p>
     `
     const resultado = articleEl.querySelector(".resultado");
     let palabra = "";
     do {
         palabra += prompt("Agregue una palabra");
-        palabra += " - "
+        if (palabra != "") {
+            palabra += " - "
+        } else {
+            alert("Debe ingresar una palabra");
+        }
     } while (confirm("Desea seguir agregando palabras?") == true);
 
-    resultado.textContent=`${palabra.slice(0, -2)}`
+    if (palabra != "" && palabra != null) {
+        resultado.textContent = `${palabra.slice(0, -2)}`
+    } else {
+        resultado.textContent = "debe ingresar alguna palabra";
+    }
 
     el.appendChild(articleEl);
 
